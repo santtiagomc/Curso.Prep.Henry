71dc480b05674c7e9040109733eb8faafaae569d
Santiago Parra <santtiagomc@gmail.com>
2022-01-18 18:01:29 -0500

    nueva iguales 3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -61,7 +61,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  return x === y;
+  return x = y;
 }
 
 function tienenMismaLongitud(str1, str2) {
