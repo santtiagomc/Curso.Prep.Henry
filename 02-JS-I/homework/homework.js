72fdf50d0ffc5b81af31f4d0f8ffe0136ff15e03
Santiago Parra <santtiagomc@gmail.com>
2022-01-18 18:03:04 -0500

    nueva longitud 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -68,7 +68,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  return str1 === str2;
+  return str1 == str2;
 }
 
 function menosQueNoventa(num) {
