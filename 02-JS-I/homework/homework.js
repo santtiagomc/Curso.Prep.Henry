5278d9dbf3260f14a0fe859e843130a3bcece948
Santiago Parra <santtiagomc@gmail.com>
2022-01-18 18:22:10 -0500

    nuevo par

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -108,7 +108,11 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 === 0) {
+   return true;
+  } else {
+    return false
+  }
 }
 
 function esImpar(num) {
